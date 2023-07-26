9cb3e95fdae065a771a2f838c221901ee57128e2
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-07-26 11:57:49 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// IMPORT
+import './App.css'
+import Container from './components/Container';
+// ________________________
+
+// COMPONENTI
+function App() {
+  return (
+    <Container>
+      <h1>Xmas Counter</h1>
+    </Container>
+  );
+}
+
+export default App
+// ____________________________
